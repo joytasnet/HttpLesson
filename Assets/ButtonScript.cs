4f417838d8bd924e670f6c6ce302d9bc399444a6
mjpurin <mjpurin@gmail.com>
2024-04-02 10:00:23 +0900

    完成

--- Assets/ButtonScript.cs
+++ Assets/ButtonScript.cs
@@ -1,18 +1,31 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.Networking;
 
-public class ButtonScript : MonoBehaviour
+public class ButtonController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public TMP_InputField et1;
+    public TMP_InputField et2;
+    public TextMeshProUGUI result;
+    
+    public void btClick() {
+        string x = et1.text;
+        string y = et2.text;
+        StartCoroutine(HttpConnect(x, y));
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    IEnumerator HttpConnect(string x,string y) {
+        WWWForm form = new WWWForm();
+        form.AddField("x", x);
+        form.AddField("y", y);
+        string url = "https://joytas.net/php/calc.php";
+        using(UnityWebRequest uwr = UnityWebRequest.Post(url, form)){
+            yield return uwr.SendWebRequest();
+            if(uwr.result != UnityWebRequest.Result.Success) {
+                Debug.Log(uwr.error);
+            } else {
+                result.text = uwr.downloadHandler.text;
+            }
+        }
     }
 }
